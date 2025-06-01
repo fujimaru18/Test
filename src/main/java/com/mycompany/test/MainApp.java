3478c14b402e2fde10a9ca0b44df884be202abca
megumikatou1255 <trnbaooo6996@gmail.com>
2025-06-01 16:40:46 +0700

    "bao fix all"

--- src/main/java/com/mycompany/test/MainApp.java
+++ src/main/java/com/mycompany/test/MainApp.java
@@ -5,7 +5,7 @@ import view.MainFrame;
 public class MainApp {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
-            new MainFrame().setVisible(true);  // ✅ Đúng
+            new MainFrame().setVisible(true);
         });
     }
 }
--- src/main/java/share/util/Validator.java
+++ src/main/java/share/util/Validator.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package share.util;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author LENOVO
- */
 public class Validator {
     public static boolean isNotEmpty(String value, String fieldName) {
         if (value == null || value.trim().isEmpty()) {
